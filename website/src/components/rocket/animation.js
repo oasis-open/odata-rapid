fd22f718e6d35cb0a2762c29ff8f4f66b01eab30
Wojtek Trocki <wtrocki@redhat.com>
2020-06-18 22:39:59 +0100

    Fix website SSR issues (#79)

--- website/src/components/rocket/animation.js
+++ website/src/components/rocket/animation.js
@@ -1,24 +1,25 @@
 // Creates animation of the RAPID SPACEMAN
 export default function rocketAnimation() {
-    var jetBubbles = document.getElementsByClassName("rocket_svg__jetBubble");
-    var astronaut = document.querySelector(".rocket_svg__astronaut");
+    if (typeof document === "undefined") {
+        return;
+    }
+    var jetBubbles = document.getElementsByClassName('rocket_svg__jetBubble');
+    var astronaut = document.querySelector('.rocket_svg__astronaut');
     var star = document.querySelector('.rocket_svg__star');
     var starContainer = document.querySelector('.rocket_svg__starContainer');
-    
+
     TweenMax.to(astronaut, 0.05, {
-        y: "+=4",
+        y: '+=4',
         repeat: -1,
         yoyo: true,
     });
     var mainTimeline = new TimelineMax({ repeat: -1 });
-    var mainSpeedLinesTimeline = new TimelineMax({ repeat: -1, paused: false });
-
     mainTimeline.timeScale(6).seek(100);
 
     function createJets() {
         TweenMax.set(jetBubbles, {
             attr: {
-                r: "-=5",
+                r: '-=5',
             },
         });
         //jet bubbles
@@ -27,12 +28,12 @@ export default function rocketAnimation() {
             var tl = new TimelineMax({ repeat: -1 });
             tl.to(jb, 1, {
                 attr: {
-                    r: "+=15",
+                    r: '+=15',
                 },
                 ease: Linear.easeNone,
             }).to(jb, 1, {
                 attr: {
-                    r: "-=15",
+                    r: '-=15',
                 },
                 ease: Linear.easeNone,
             });
@@ -58,7 +59,7 @@ export default function rocketAnimation() {
                     repeat: -1,
                     repeatDelay: 1,
                     ease: Linear.easeNone,
-                }
+                },
             );
         }
     }
