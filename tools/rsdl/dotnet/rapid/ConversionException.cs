3fc790be9c98d9b9bb2e2416639b0c358c144387
Christof Sprenger <53094940+chrisspre@users.noreply.github.com>
2020-09-10 06:50:28 -0700

    some error handling in cli (#110)

---
+++ tools/rsdl/dotnet/rapid/ConversionException.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace rsdl.parser
+{
+    [Serializable]
+    internal class ConversionException : Exception
+    {
+        public int ReturnCode { get; }
+
+        public ConversionException(int returnCode)
+        {
+            ReturnCode = returnCode;
+        }
+
+        public ConversionException(int returnCode, string message) : base(message)
+        {
+            ReturnCode = returnCode;
+        }
+
+        public ConversionException(int returnCode, string message, Exception innerException) : base(message, innerException)
+        {
+            ReturnCode = returnCode;
+        }
+
+        protected ConversionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
