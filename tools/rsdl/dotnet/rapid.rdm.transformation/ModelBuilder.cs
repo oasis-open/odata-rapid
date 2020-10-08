d0534a389bd6c03963cb3843c4934d9490fbb880
Christof Sprenger <53094940+chrisspre@users.noreply.github.com>
2020-10-08 08:26:08 -0700

    container name (#137)
    * default container name  uppercase initial "Service"

--- tools/rsdl/dotnet/rapid.rdm.transformation/ModelBuilder.cs
+++ tools/rsdl/dotnet/rapid.rdm.transformation/ModelBuilder.cs
@@ -188,7 +188,7 @@ namespace rapid.rsdl
 
         private void AddService(RdmService service)
         {
-            var containerName = "default";
+            var containerName = "Service";
             var container = (EdmEntityContainer)edmModel.EntityContainer ?? edmModel.AddEntityContainer(namespaceName, containerName);
 
             foreach (var item in service.Items)
