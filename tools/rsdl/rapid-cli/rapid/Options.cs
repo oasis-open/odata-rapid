dd97352bc32a62b995478744c674eea097f94e5b
Christof Sprenger <chrispre@microsoft.com>
2021-01-25 15:22:07 -0800

    fix command line arguments

--- tools/rsdl/rapid-cli/rapid/Options.cs
+++ tools/rsdl/rapid-cli/rapid/Options.cs
@@ -6,16 +6,16 @@ namespace rapid
     {
         public class Options
         {
+            [Value(0, MetaName = "rsdl file", Required = true, HelpText = "Input file including path")]
+            public string InputPath { get; set; }
+
             [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose { get; set; }
-
-            [Value(0, MetaName = "rsdl file", HelpText = "Input file including path")]
-            public string InputPath { get; set; }
 
             [Option('f', "format", Required = false, HelpText = "Specify the format of the generated CSDL. One of JSON, XML or both.", Default = CsdlFormat.All)]
             public CsdlFormat Format { get; set; }
 
-            [Option('o', "output", Required = false, HelpText = "Output direcotry. Defaults to input file's directory.")]
+            [Option('o', "output", Required = false, HelpText = "Output directory. Defaults to input file's directory.")]
             public string OutputPath { get; set; }
         }
     }
