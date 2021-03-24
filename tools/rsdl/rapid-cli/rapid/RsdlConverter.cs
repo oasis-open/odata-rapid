2c10b5c7dbb02179bc413a7116ec6218d1599608
Michael Pizzo <mikep@microsoft.com>
2021-03-24 15:21:41 -0700

    Support reading/writing to/from stream

--- tools/rsdl/rapid-cli/rapid/RsdlConverter.cs
+++ tools/rsdl/rapid-cli/rapid/RsdlConverter.cs
@@ -5,11 +5,10 @@ using Microsoft.OData.Edm;
 using System.Linq;
 using rapid.rsdl;
 using rapid.rdm;
-using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.OData;
-using Microsoft.OData.Edm.Vocabularies;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi;
+using System.Threading.Tasks;
 
 namespace rapid
 {
@@ -20,7 +19,7 @@ namespace rapid
     {
         public RsdlConverter(ILogger logger)
         {
-            this.logger = logger;
+            this.logger = logger ?? NullLogger.Instance;
         }
 
         public readonly ILogger logger;
@@ -28,9 +27,47 @@ namespace rapid
         /// <summary>
         /// Converts a RAPID pro schema definition language file (.rsdl) to a CSDL file
         /// </summary>
+        /// <param name="inputRsdl">reader to read the RSDL to parse</param>
+        /// <param name="output">stream to write the output to</param>
+        /// <param name="format">indicates whether it should be written as XML or JSON CSDL, or OpenAPI</param>
+        /// <param name="modelName">the name of the model being converted</param>
+        public async Task<bool> Convert(TextReader inputRsdl, Stream output, OutputFormat format, string modelName = "RapidModel")
+        {
+            // create parser and model transformer.
+            RdmDataModel model;
+            var parser = new RdmParser(logger);
+
+            try
+            {
+                var rsdl = await inputRsdl.ReadToEndAsync();
+                model = parser.Parse(rsdl, modelName);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"error parsing rsdl stream");
+                return false;
+            }
+
+            // todo: Load referenced EDM models
+
+            // transform into CSDL
+            var transformer = new ModelTransformer(logger);
+            if (transformer.TryTransform(model, null, out var csdlModel))
+            {
+                return await WriteOutput(csdlModel, output, format);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Converts a RAPID pro schema definition language file (.rsdl) to a CSDL file
+        /// </summary>
         /// <param name="path">file path of input file to parse</param>
-        /// <param name="format">indicates whether it should be written as XML or JSON CSDL</param>
-        public bool Convert(string path, OutputFormat format)
+        /// <param name="format">indicates whether it should be written as XML or JSON CSDL, or OpenAPI</param>
+        public async Task<bool> Convert(string path, OutputFormat format)
         {
             // create parser and model transformer.
             var parser = new RdmParser(logger);
@@ -54,8 +91,7 @@ namespace rapid
             // transform into CSDL
             if (transformer.TryTransform(model, referencedModels, out var csdlModel))
             {
-                WriteCsdl(csdlModel, path, format);
-                return true;
+                return await WriteOutputToFile(csdlModel, path, format);
             }
             else
             {
@@ -95,8 +131,32 @@ namespace rapid
             }
         }
 
-        private void WriteCsdl(IEdmModel model, string inputPath, OutputFormat format = OutputFormat.XML, bool verbose = false)
+        private async Task<bool> WriteOutput(IEdmModel model, Stream stream, OutputFormat format)
         {
+            if (format == OutputFormat.XML)
+            {
+                await WriteXml(model, stream);
+            }
+            else if (format == OutputFormat.JSON)
+            {
+                await WriteJson(model, stream);
+            }
+            else if (format == OutputFormat.OpenAPI)
+            {
+                await WriteOpenApi (model, stream);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private async Task<bool> WriteOutputToFile(IEdmModel model, string inputPath, OutputFormat format = OutputFormat.XML, bool verbose = false)
+        {
+            bool wroteOutput = false;
+
             if (format.HasFlag(OutputFormat.XML))
             {
                 var path = Path.ChangeExtension(inputPath, ".csdl.xml");
@@ -104,11 +164,11 @@ namespace rapid
                 {
                     logger.LogInfo("writing {0}", path);
                 }
-                using (var file = File.CreateText(path))
-                using (var writer = System.Xml.XmlWriter.Create(file, new System.Xml.XmlWriterSettings { Indent = true }))
+                using (var file = File.Create(path))
                 {
-                    CsdlWriter.TryWriteCsdl(model, writer, CsdlTarget.OData, out var errors);
+                    await WriteXml(model, file);
                 }
+                wroteOutput = true;
             }
             if (format.HasFlag(OutputFormat.JSON))
             {
@@ -118,39 +178,83 @@ namespace rapid
                     logger.LogInfo("writing {0}", path);
                 }
                 using (var file = File.Create(path))
-                using (var jsonWriter = new System.Text.Json.Utf8JsonWriter(file, new System.Text.Json.JsonWriterOptions { Indented = true }))
                 {
-                    CsdlWriter.TryWriteCsdl(model, jsonWriter, out var errors);
+                    await WriteJson(model, file);
                 }
+                wroteOutput = true;
             }
             if (format.HasFlag(OutputFormat.OpenAPI))
             {
-                string title = "RAPID Service";
-                string description = null;
-                string version = null;
-                if (model.EntityContainer != null)
-                {
-                    title = model.EntityContainer.Name ?? title;
-                    description = model.GetDescriptionAnnotation(model.EntityContainer) ?? "RAPID Service for namespace " + model.EntityContainer.Namespace;
-                    version = model.GetAnnotationValue(model.EntityContainer, "Org.OData.Core.V1", "SchemaVersion") as string;
-                }
                 var path = Path.ChangeExtension(inputPath, ".openapi");
                 if (verbose)
                 {
                     logger.LogInfo("writing {0}", path);
                 }
-                using (var file = new StreamWriter(path))
+                using (var file = File.Create(path))
                 {
-                    var document = model.ConvertToOpenApi();
-                    document.Servers.Clear();
-                    document.Info.Title = title;
-                    document.Info.Description = description;
-                    document.Info.Version = version;
-                    var openApi = document.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0);
-                    file.WriteLine(openApi);
-                    file.Flush();
-                    file.Close();
+                    await WriteOpenApi(model, file);
                 }
+                wroteOutput = true;
+            }
+
+            return wroteOutput;
+        }
+
+        private static async Task WriteXml(IEdmModel model, Stream stream)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (var xmlWriter = System.Xml.XmlWriter.Create(memoryStream, new System.Xml.XmlWriterSettings { Indent = true }))
+            {
+                // Update once CsdlWriter adds an async overload
+                if (CsdlWriter.TryWriteCsdl(model, xmlWriter, CsdlTarget.OData, out var errors))
+                {
+                    // until CsdlWriter supports writing async, write to a memory stream and copyasync to the target stream
+                    // alternatively, could wrap the stream in an async wrapper
+                    memoryStream.Seek(0, 0);
+                    await memoryStream.CopyToAsync(stream);
+                }
+            }
+        }
+
+        private static async Task WriteJson(IEdmModel model, Stream stream)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (var jsonWriter = new System.Text.Json.Utf8JsonWriter(memoryStream, new System.Text.Json.JsonWriterOptions { Indented = true }))
+            {
+                if (CsdlWriter.TryWriteCsdl(model, jsonWriter, out var errors))
+                {
+                    // until CsdlWriter supports writing async, write to a memory stream and copyasync to the target stream
+                    // alternatively, could wrap the stream in an async wrapper
+                    memoryStream.Seek(0, 0);
+                    await memoryStream.CopyToAsync(stream);
+                }
+            }
+        }
+
+        private static async Task WriteOpenApi(IEdmModel model, Stream stream)
+        {
+            string title = "RAPID Service";
+            string description = null;
+            string version = null;
+            if (model.EntityContainer != null)
+            {
+                title = model.EntityContainer.Name ?? title;
+                description = model.GetDescriptionAnnotation(model.EntityContainer) ?? "RAPID Service for namespace " + model.EntityContainer.Namespace;
+                version = model.GetAnnotationValue(model.EntityContainer, "Org.OData.Core.V1", "SchemaVersion") as string;
+            }
+
+            var document = model.ConvertToOpenApi();
+            document.Servers.Clear();
+            document.Info.Title = title;
+            document.Info.Description = description;
+            document.Info.Version = version;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // since SerializeAsJson is synchronous write to a memory stream and copyasync to the target stream
+                // alternatively, could wrap the stream in an async wrapper
+                document.SerializeAsJson(memoryStream, OpenApiSpecVersion.OpenApi3_0);
+                memoryStream.Seek(0, 0);
+                await stream.CopyToAsync(stream);
             }
         }
     }
