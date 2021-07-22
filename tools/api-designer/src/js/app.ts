f1bd52d08c672dcddfb802dbe7f1733a77d7b114
Clement Habinshuti <clhabins@microsoft.com>
2021-07-22 17:07:10 +0300

    Implement api designer

---
+++ tools/api-designer/src/js/app.ts
@@ -0,0 +1,52 @@
+class App {
+    listeners: Listener[];
+    rsdl: string;
+    requestUrl: string;
+    constructor() {
+        this.listeners = [];
+    }
+    /**
+     * @param {Listener} listener 
+     */
+    addListener = (listener) => {
+        this.listeners.push(listener);
+    }
+    /**
+     * 
+     * @param {string} senderAddress 
+     * @param {string} event 
+     */
+    publishRsdl = (senderAddress, event) => {
+        if (event === this.rsdl)
+        {
+            return;
+        }
+        this.rsdl = event;
+        this.listeners.forEach(listener => {
+            if (listener.senderAddress !== senderAddress) {
+                listener.callback(event);
+            }
+        });
+    }
+}
+
+type ListenerCallback = (rsdl: string) => any;
+
+class Listener {
+    senderAddress: string;
+    config: any;
+    callback: (rsdl: string) => any;
+    /**
+     * 
+     * @param {string} senderAddress 
+     */
+    constructor(senderAddress, callback: ListenerCallback = () => {}) {
+        this.senderAddress = senderAddress;
+        this.callback = callback;
+    }
+
+    configure = (config) => {
+        this.config = config;
+    }
+}
+export { App, Listener };
