3e17f46306ba331514d84ebd83595e9c03eb6f86
Ralf Handl <ralf.handl@sap.com>
2022-07-26 14:09:57 +0200

    Align package names

--- tools/visualModelEditor/webpack.config.js
+++ tools/visualModelEditor/webpack.config.js
@@ -2,42 +2,43 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/index.ts",
-    devtool: "inline-source-map",
-    stats: { errorDetails: true },
-    module: {
-        rules: [
-            {
-                test: /\.ts/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ]
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  stats: { errorDetails: true },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      util: false,
+      assert: false,
+      fs: false,
     },
-    resolve: {
-        extensions: ['.ts', '.js'],
-        fallback: {
-            util: false,
-            assert: false,
-            fs: false,
-        }
+  },
+  output: {
+    clean: true,
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    library: {
+      name: "visualModelEditor",
+      type: "umd",
     },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: {
-            name: "visualModelEditor",
-            type: "umd"
-        }
-    },
-    // plugins: [
-    //     new webpack.ProvidePlugin({
-    //         process: "process/browser"
-    //     })
-    // ],
-    mode: "development"
-}
+  },
+  // plugins: [
+  //     new webpack.ProvidePlugin({
+  //         process: "process/browser"
+  //     })
+  // ],
+  mode: "development",
+};
